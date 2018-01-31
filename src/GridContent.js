107628deef3d6cfd32bfd90c990fbc95712353e6
Marko Crnobrnja <fdsafk@jiaofga.com>
2018-01-31 03:15:54 +0100

    tekst

--- src/GridContent.js
+++ src/GridContent.js
@@ -373,15 +373,13 @@ export default withAuth (class GridContent extends React.Component {
             To do so you need to select the cell that you wish to write the result to and enter the formula of the function
           </div>
           <div className="col-lg-3 col-lg-3 col-xl-3 functionsPanelLabel">
-            For example: B3 + max(A1,C4 ^3) <br/>
-            Or: if(tan D1 %lt 1, D1, atan(1-D1))
+            For example: <span style="color:green"> B3 + max(A1,C4 ^3) </span> <br/>
+            Or: <span stlye="color:green"> if(tan D1 &lt 1, D1, atan(1-D1)) </span>
+            
           </div>
           <div className="col-lg-3 col-lg-3 col-xl-3 functionsPanelLabel">
              All basic arithmetic and trigonometric functions and operators are available, as well as string concatenation with "||" and string lenghth with "length"
-          </div>
-          <div className="col-lg-3 col-lg-3 col-xl-3 functionsPanelLabel">
              See <a href="https://github.com/silentmatt/expr-eval">here</a> for the complete list. 
-          </div>
                 
               
           </div>;
